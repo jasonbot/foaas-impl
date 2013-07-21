8ef3a609305ca0c9f5f873e8d2587c00fc1eb95a
"Jason Scheirer" <jason.scheirer@gmail.com>
2013-07-20 17:48:06 -0700

    Refactor for cleaner code

--- foaas.py
+++ foaas.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import functools
+import re
 
 import bottle
 
@@ -14,8 +14,8 @@ bootstrap_page = """
   </head>
   <body>
     <div class="hero-unit">  
-      <h1>{}</h1>
-      <p>{}</p>
+      <h1>{message}</h1>
+      <p>- {subtitle}</p>
     </div>
   </body>
 </html>
@@ -32,22 +32,23 @@ def return_content_type():
     for ret in return_type.split(','):
         if ret in ('text/plain', 'application/json', 'text/html'):
             return ret
-    return 'text/plain
+    return 'text/plain'
 
 def content_type_adjuster(fn):
-    @functools.wraps(fn)
     def _fn(*a, **k):
         return_type = return_content_type()
         return_value = fn(*a, **k)
         bottle.response.set_header('Content-Type', return_type)
+        message, subtitle = (return_value.rsplit(" - ", 1) + [""])[:2]
+
         if return_type == "text/plain":
             return return_value
         elif return_type == "application/json":
             items = return_value.rsplit(" - ", 1) + [""]
-            return { 'message': items[0], 'subtitle': items[1] }
+            return { 'message': message, 'subtitle': subtitle }
         elif return_type == "text/html":
             items = return_value.rsplit(" - ", 1) + [""]
-            return bootstrap_page.format(items[0], items[1])
+            return bootstrap_page.format(message=message, subtitle=subtitle)
     return _fn
 
 class TextRoute(object):
@@ -57,6 +58,12 @@ class TextRoute(object):
     @content_type_adjuster
     def __call__(self, **keyword_args):
         return self._text.format(**keyword_args)
+
+def fix_routes(routes):
+    variable_re = re.compile("[:]([a-z]+)")
+    for route_path, route_text in routes:
+        yield (variable_re.sub("<\\1>", route_path),
+               variable_re.sub("{\\1}", route_text))
 
 routes = (
     ("/off/:name/:from", "Fuck off, :name. - :from"),
@@ -84,6 +91,9 @@ routes = (
     ("/:thing/:from", "Fuck :thing. - :from"),
     ("/thanks/:from", "Fuck you very much. - :from"),
 )
+
+for route_path, route_text in fix_routes(routes):
+    TextRoute(foaas_app, route_path, route_text)
 
 if __name__ == "__main__":
     bottle.run(app=foaas_app, host='0.0.0.0', port=8088, debug=True)
